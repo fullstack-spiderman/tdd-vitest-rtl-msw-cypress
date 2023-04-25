e5e26f21b472b88e9e81c9257c575082a77ad8c0
Arjun KayalMoni <arjun.umathanu@gmail.com>
2023-04-25 16:39:16 +0530

    basic setup template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
+import Greet from '@/components/Greet'
+
 /* eslint-disable react/button-has-type */
 export default function App({ title }) {
   return (
     <>
+      <Greet />
       <div>{title || 'Hello World'}</div>
       <input type="text" />
       <a href="#">some link</a>
-      <button>Greet</button>
+      <button>Greetings</button>
     </>
   )
 }
---
+++ src/components/Greet.jsx
@@ -0,0 +1,3 @@
+export default function Greet() {
+  return <div>Greetings from Vitest and Vite</div>
+}
